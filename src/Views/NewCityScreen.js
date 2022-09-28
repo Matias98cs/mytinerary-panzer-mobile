b15b271a71a9d453415d23d550911462a660d693
Matias98cs <matias98cs@gmail.com>
2022-09-28 18:02:24 -0300

    se agrego la pagin de new city

--- src/Views/NewCityScreen.js
+++ src/Views/NewCityScreen.js
@@ -1,26 +1,42 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import { ScrollView } from 'react-native-gesture-handler'
+import React from "react";
+import { View, Text, StyleSheet, ImageBackground } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import FormNewCity from "../components/forms/FormNewCity";
+
+const image = {
+  uri: "https://images.pexels.com/photos/12123082/pexels-photo-12123082.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+};
 
 const NewCityScreen = () => {
   return (
-    <ScrollView style={styles.scrollStyle}>
-      <Text style={{
-        fontSize: 40,
-        textAlign: 'center',
-        marginTop: "20%"
-      }}>
-        New City 🧳
-      </Text>
-
-    </ScrollView>
-  )
-}
+    // <ScrollView style={styles.scrollStyle}>
+      <View style={styles.containeNewCity}>
+        <ImageBackground
+          source={image}
+          resizeMode="cover"
+          style={styles.imgBack}
+        >
+          <Text style={{fontSize: 40, paddingVertical: 10, color: "#eee"}}>Add Your City</Text>
+          <FormNewCity />
+        </ImageBackground>
+      </View>
+    // </ScrollView>
+  );
+};
 
 const styles = StyleSheet.create({
   scrollStyle: {
-    backgroundColor: '#DCD7C9',
-  }
+    backgroundColor: "#DCD7C9",
+  },
+  containeNewCity: {
+    width: '100%',
+    height: '100%',
+  },
+  imgBack: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: 'center'
+  },
 });
 
-export default NewCityScreen
+export default NewCityScreen;
---
+++ src/components/forms/FormNewCity.js
@@ -0,0 +1,57 @@
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native'
+import React from 'react'
+
+
+export default function FormNewCity() {
+  return (
+    <View style={styles.containerInputs}>
+      <View>
+        <TextInput style={styles.inputText} placeholder='City'/>
+      </View>
+      <View>
+        <TextInput style={styles.inputText} placeholder='Country'/>
+      </View>
+      <View>
+        <TextInput style={styles.inputText} placeholder='Photo'/>
+      </View>
+      <View>
+        <TextInput style={styles.inputText} placeholder='Population'/>
+      </View>
+      <View>
+        <TextInput keyboardType='numeric' style={styles.inputText} placeholder='Fundation'/>
+      </View>
+      <View>
+        <TextInput style={styles.inputText} placeholder='Description'/>
+      </View>
+      <View style={styles.btnCreate}>
+        <Button title='Send' color="#eee"/>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    containerInputs: {
+        display: 'flex',
+        justifyContent: "space-between" ,
+        height: "70%",
+        width: "90%",
+        borderRadius: 14,
+        padding: 12
+    },
+    inputText: {
+        padding: 10,
+        height: 45,
+        borderRadius: 14,
+        fontSize: 22,
+        backgroundColor: '#DCD7C9'
+    },
+    btnCreate: {
+        display: 'flex',
+        backgroundColor: '#A27B5C',
+        width: '50%',
+        justifyContent: 'center',
+        alignSelf: 'center',
+        borderRadius: 14,
+    }
+})
